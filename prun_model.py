a1cac5a82ee0cde70d23b5100bc3899133e17852
Rohit Mohan <rohitmohan1914@gmail.com>
2019-07-19 10:16:31 +0200

    Update prun_model.py

--- prun_model.py
+++ prun_model.py
@@ -1,3 +1,17 @@
+''' AdapNet++:  Self-Supervised Model Adaptation for Multimodal Semantic Segmentation
+
+ Copyright (C) 2018  Abhinav Valada, Rohit Mohan and Wolfram Burgard
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.'''
+
 import importlib
 import json
 import numpy as np
